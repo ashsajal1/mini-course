95b3fa186d315fe3949ba8ea9c30d8940cbb0901
Ashsajal <ashsajal1@gmail.com>
2025-10-31 22:04:38 +0600

    feat: add edit btn

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -137,7 +137,17 @@ export default async function CoursePage({
             </div>
           </div>
 
-          <div className="card-actions justify-end mt-8">
+          <div className="card-actions justify-end gap-2 mt-8">
+            <Link
+              href={`/course/edit/${id}`}
+              className="btn btn-outline gap-2"
+            >
+              Edit Course
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4">
+                <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+                <path d="m15 5 4 4" />
+              </svg>
+            </Link>
             <Link
               href={`/course/learn/${id}`}
               className="btn btn-primary gap-2"
