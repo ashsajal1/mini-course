46b38fae2bdbf71d2e3d3dbe6048bc7227275be7
Ashsajal <ashsajal1@gmail.com>
2025-11-12 18:09:17 +0600

    ✨ feat: protect course create page

--- app/course/create/page.tsx
+++ app/course/create/page.tsx
@@ -1,5 +1,11 @@
 import CourseForm from "./course-form";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function CreateCoursePage() {
+export default async function CreateCoursePage() {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in?redirect_url=/course/create");
+  }
   return <CourseForm />;
 }
