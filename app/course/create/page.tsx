06af2653d74d969e821a41a6a783d60b8925db6b
Ashsajal <ashsajal1@gmail.com>
2025-11-12 18:19:30 +0600

    ✨ feat: remove auth check

--- app/course/create/page.tsx
+++ app/course/create/page.tsx
@@ -1,11 +1,5 @@
 import CourseForm from "./course-form";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
 export default async function CreateCoursePage() {
-  const { userId } = await auth();
-  if (!userId) {
-    redirect("/sign-in?redirect_url=/course/create");
-  }
   return <CourseForm />;
 }
