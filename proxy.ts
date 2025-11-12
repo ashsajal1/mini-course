7e5db3f019430bd35a44fddafb5c566dba476e31
Ashsajal <ashsajal1@gmail.com>
2025-11-12 18:06:03 +0600

    ✨ feat: make home, about, contact public

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/",
+  "/about",
+  "/contact",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/course",
