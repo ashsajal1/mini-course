a743789ef67952accc90f3f72c1d51d4b7147704
Ashsajal <ashsajal1@gmail.com>
2025-11-23 09:00:56 +0600

    ✨ feat: remove creator field

--- app/course/create/page.tsx
+++ app/course/create/page.tsx
@@ -1,9 +1,5 @@
-import { currentUser } from "@clerk/nextjs/server";
 import CourseForm from "./course-form";
 
 export default async function CreateCoursePage() {
-  const user = await currentUser();
-  const userName = user?.fullName || "";
-
-  return <CourseForm userName={userName} />;
+  return <CourseForm />;
 }
