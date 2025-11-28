9c52a2e13f97fce969b545d7e18c5dcb1311d1d6
Ashsajal <ashsajal1@gmail.com>
2025-11-28 14:58:56 +0600

    chore: add database migration for enrollment model

---
+++ prisma/migrations/20251128085702_add_enrollment_model/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Enrollment" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "course_id" TEXT NOT NULL,
+    "enrolled_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "completed_at" TIMESTAMP(3),
+    "is_completed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Enrollment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Enrollment_user_id_course_id_key" ON "Enrollment"("user_id", "course_id");
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_course_id_fkey" FOREIGN KEY ("course_id") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
